4a0a79e47091e7128ff5369c78315dc8c0402ef9
Ajay Pratap Singh <singhajaypratap606@gmail.com>
2023-08-18 12:52:29 +0530

    update settings for Deployment

--- backend/urls.py
+++ backend/urls.py
@@ -60,3 +60,6 @@ urlpatterns = [
     path("userApi/", include("userApi.urls")),
     path("vendorApi/", include("vendor.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
