da766b045d00b35d98b3563803c9dd0588de1ddf
Ajay Pratap Singh <singhajaypratap606@gmail.com>
2023-08-18 12:34:36 +0530

    versel settings update

--- backend/urls.py
+++ backend/urls.py
@@ -16,48 +16,47 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
 
 from rest_framework_simplejwt import views as jwt_views
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
 schema_view = get_schema_view(
-   openapi.Info(
-      title="MobileKart Rest APIs",
-      default_version='v1',
-      description="This is a Ecommerce rest api application",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="singhajaypratap606@gmail.com"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="MobileKart Rest APIs",
+        default_version="v1",
+        description="This is a Ecommerce rest api application",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="singhajaypratap606@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 
-
 urlpatterns = [
-
-      path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
-
-
-       path('api/token/',
-         jwt_views.TokenObtainPairView.as_view(),
-         name ='token_obtain_pair'),
-    path('api/token/refresh/',
-         jwt_views.TokenRefreshView.as_view(),
-         name ='token_refresh'),
-
-    
-    path('admin/', admin.site.urls),
-    path('', include("home.urls") ),
-    path('adminApi/', include("adminApi.urls") ),
-    path('userApi/', include("userApi.urls") ),
-    path('vendorApi/', include("vendor.urls") ),
-
+    path(
+        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path(
+        "api/token/", jwt_views.TokenObtainPairView.as_view(), name="token_obtain_pair"
+    ),
+    path(
+        "api/token/refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"
+    ),
+    path("admin/", admin.site.urls),
+    path("", include("home.urls")),
+    path("adminApi/", include("adminApi.urls")),
+    path("userApi/", include("userApi.urls")),
+    path("vendorApi/", include("vendor.urls")),
 ]
--- home/views.py
+++ home/views.py
@@ -1,22 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import JsonResponse , HttpResponse;
-
-
+from django.http import JsonResponse, HttpResponse
 
 
 def Home(request):
 
     allPaths = {
-        'home' : '/',
-        'adminApi' : '/adminApi',
-        'userApi' : '/userApi',
-        'vendorApi' : '/vendorApi',
-           'swagger' : '/swagger',
-              'redoc' : '/redoc',
+        "home": "/",
+        "adminApi": "/adminApi",
+        "userApi": "/userApi",
+        "vendorApi": "/vendorApi",
+        "swagger": "/swagger",
+        "redoc": "/redoc",
     }
 
     # return JsonResponse({'message': 'success its working'} )
     return JsonResponse(allPaths)
-
